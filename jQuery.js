8d9f596df5895104ea4d999c9a562351a76a377b
Max <68995982+MaximusMK@users.noreply.github.com>
2020-09-17 19:54:28 +0300

    Add files via upload

---
+++ jQuery.js
@@ -0,0 +1,5 @@
+jQuery(function() {...});, или $(function() {...});
+// Пример как добавить библиотеку. Размещайте ссылки на jQuery-файл внутри тега <head>.
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+                                             
